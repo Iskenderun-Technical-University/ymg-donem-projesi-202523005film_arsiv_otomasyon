889d3da886326faaa60985f291406f3566e93806
bledabilir <bleda02@hotmail.com>
2023-06-01 00:13:03 +0300

    admin_side %66 complated and sql connection complated

--- movie_archive/src/application/admin_sidecontroller.java
+++ movie_archive/src/application/admin_sidecontroller.java
@@ -9,6 +9,8 @@ import java.util.ResourceBundle;
 import com.moviearchiveSQL.Util.databaseUtil;
 
 import application.admin_sidecontroller.Kayitlar;
+import application.admin_sidecontroller.kayitlar2;
+import application.admin_sidecontroller.kayitlar3;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,6 +19,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -51,12 +55,15 @@ public class admin_sidecontroller {
 
     @FXML
     private Button btn4;
+    
+    @FXML
+    private TableColumn<kayitlar3, Integer> comment_id;
+    
+    @FXML
+    private TableColumn<kayitlar3, String> commennts;
 
     @FXML
-    private TableColumn<?, ?> commennts;
-
-    @FXML
-    private TableView<?> comments;
+    private TableView<kayitlar3> comments;
 
     @FXML
     private TableColumn<Kayitlar, Double> imdb_point;
@@ -74,34 +81,37 @@ public class admin_sidecontroller {
     private TableColumn<Kayitlar, String> movie_name;
 
     @FXML
-    private TableColumn<?, ?> points;
+    private TableColumn<kayitlar3, Double> point;
 
     @FXML
     private TableColumn<Kayitlar, String> subject;
 
     @FXML
-    private TableColumn<?, ?> user1_id;
+    private TableColumn<kayitlar2, Integer> user1_id;
 
     @FXML
-    private TableColumn<?, ?> user1_name;
+    private TableColumn<kayitlar2, String> user1_name;
 
     @FXML
-    private TableColumn<?, ?> user_id;
+    private TableColumn<kayitlar3, Integer> user_id;
 
     @FXML
-    private TableView<?> user_inf;
+    private TableView<kayitlar2> user_inf;
 
     @FXML
-    private TableColumn<?, ?> user_mail;
+    private TableColumn<kayitlar2, String> user_mail;
 
     @FXML
     private TableColumn<Kayitlar, String> link;
     
     @FXML
-    private TableColumn<?, ?> user_pasword;
+    private TableColumn<kayitlar2, String> user_pasword;
 
     @FXML
-    private TableColumn<?, ?> usr_name;
+    private TableColumn<kayitlar3, String> usr_name;
+    
+    @FXML
+    private TableColumn<kayitlar3, Integer> movie1_id;
 
     @FXML
     private Button delete;
@@ -111,6 +121,20 @@ public class admin_sidecontroller {
     
     @FXML
     private Button update;
+    
+    @FXML
+    private TextField lnk;
+    
+    @FXML
+    private TextArea sub;
+    
+    @FXML
+    private TextField film;
+
+    @FXML
+    private TextField imdb;
+
+    
     
     @FXML
     private VBox vbox;
@@ -139,9 +163,62 @@ public class admin_sidecontroller {
     	
     }
     
+    public void degergetir2(TableView<kayitlar2> tablo1) {
+    	sql="select * from login_form";
+    	ObservableList<kayitlar2> kayitlarliste2=FXCollections.observableArrayList();
+    	try {
+    		sorgu=baglanti.prepareStatement(sql);
+    		ResultSet getirilen=sorgu.executeQuery();
+    		while(getirilen.next()) {
+    			kayitlarliste2.add(new kayitlar2(getirilen.getInt("user_id"),getirilen.getString("user_name"),getirilen.getString("user_mail"),getirilen.getString("user_password")));
+    			user1_id.setCellValueFactory(new PropertyValueFactory<>("user1_id"));
+    			user1_name.setCellValueFactory(new PropertyValueFactory<>("user1_name"));
+    			user_mail.setCellValueFactory(new PropertyValueFactory<>("user_mail"));
+    			user_pasword.setCellValueFactory(new PropertyValueFactory<>("user_pasword"));
+    			
+    			
+    		}
+    		
+    	
+    		user_inf.setItems(kayitlarliste2);
+    		
+		} catch (Exception e) {
+		System.out.println("hata");
+		}
+    	
+    	
+    }
     
+    public void degergetir3(TableView<kayitlar3> tablo3) {
+    	sql="select * from comminicate";
+    	ObservableList<kayitlar3> kayitlarliste3=FXCollections.observableArrayList();
+    	try {
+    		sorgu=baglanti.prepareStatement(sql);
+    		ResultSet getirilen=sorgu.executeQuery();
+    		while(getirilen.next()) {
+    			kayitlarliste3.add(new kayitlar3(getirilen.getInt("comment_id"), getirilen.getInt("movie1_id"),getirilen.getInt("user_id"),getirilen.getString("usr_name"),getirilen.getString("commennts"),getirilen.getDouble("point")));
+    			comment_id.setCellValueFactory(new PropertyValueFactory<>("comment_id"));
+    			movie1_id.setCellValueFactory(new PropertyValueFactory<>("movie1_id"));
+    			user_id.setCellValueFactory(new PropertyValueFactory<>("user_id"));
+    			usr_name.setCellValueFactory(new PropertyValueFactory<>("usr_name"));
+    			commennts.setCellValueFactory(new PropertyValueFactory<>("commennts"));
+    			point.setCellValueFactory(new PropertyValueFactory<>("point"));
+    			
+    			
+    			
+    		}
+    		
+    	
+    		comments.setItems(kayitlarliste3);
+    		
+		} catch (Exception e) {
+		System.out.println("hata");
+		}
+    	
+    	
+    }
     
-    
+
     
 
     @FXML
@@ -150,6 +227,7 @@ public class admin_sidecontroller {
     	user_inf.setVisible(false);
     	movie.setVisible(false);
     	comments.setVisible(true);
+    	degergetir3(comments);
     }
 
     @FXML
@@ -157,6 +235,7 @@ public class admin_sidecontroller {
     	comments.setVisible(false);
     	movie.setVisible(false);
     	user_inf.setVisible(true);
+    	degergetir2(user_inf);
     }
 
     @FXML
@@ -164,6 +243,8 @@ public class admin_sidecontroller {
     	comments.setVisible(false);
     	user_inf.setVisible(false);
     	movie.setVisible(true);
+    	degergetir(movie);
+    
     }
 
     @FXML
@@ -173,12 +254,26 @@ public class admin_sidecontroller {
     
     @FXML
     void delete_click(ActionEvent event) {
-
+    
     }
 
     @FXML
     void inser_click(ActionEvent event) {
-
+    	sql="insert into movies_inf(movie_name,imdb_point,subject,movie_link) values(?,?,?,?)";
+    	try {
+    		sorgu=baglanti.prepareStatement(sql);
+    		sorgu.setString(1, film.getText().trim());
+    		sorgu.setString(2, imdb.getText().trim());
+    		sorgu.setString(3, sub.getText().trim());
+    		sorgu.setString(4, lnk.getText().trim());
+    		sorgu.executeUpdate();
+    		degergetir(movie);
+    		
+    
+    	}
+    	catch(Exception e) {
+    		
+    		}
     }
 
     @FXML
@@ -186,13 +281,16 @@ public class admin_sidecontroller {
 
     }
 
+
+    
     @FXML
     void initialize() {
     	comments.setVisible(false);
     	user_inf.setVisible(false);
-    	degergetir(movie);
-    	link.setVisible(true);
-    
+    	movie.setVisible(false);
+    	
+    	
+ 
 
 
     }
@@ -202,6 +300,7 @@ public class admin_sidecontroller {
     	private double imdb_point;
     	private String subject;
     	private String movie_link;
+    	
     	 public int getMovie_id() {
 			return movie_id;
 		}
@@ -243,7 +342,133 @@ public class admin_sidecontroller {
  			this.movie_link=link;
  		}
 
-	
+    	 
     }
+    
+    public static class kayitlar2{
+    	private int user1_id;
+    	private String user1_name;
+    	private String user_mail;
+    	private String user_pasword;
+    	
+    	 kayitlar2() {
+    		 
+    		
+    	}
+    	 
+    	 
+    	 kayitlar2(int user1_id,String user1_name,String user_mail,String user_pasword) {
+   			this.user1_id=user1_id;
+   			this.user1_name=user1_name;
+   			this.user_mail=user_mail;
+   			this.user_pasword=user_pasword;
+    	 }
+
+
+		public int getUser1_id() {
+			return user1_id;
+		}
+
+
+		public void setUser1_id(int user1_id) {
+			this.user1_id = user1_id;
+		}
+
+
+		public String getUser1_name() {
+			return user1_name;
+		}
+
+
+		public void setUser1_name(String user1_name) {
+			this.user1_name = user1_name;
+		}
+
+
+		public String getUser_mail() {
+			return user_mail;
+		}
+
+
+		public void setUser_mail(String user_mail) {
+			this.user_mail = user_mail;
+		}
+
+
+		public String getUser_pasword() {
+			return user_pasword;
+		}
+
+
+		public void setUser_pasword(String user_pasword) {
+			this.user_pasword = user_pasword;
+		}
+    	
+    }
+    
+    
+    public static class kayitlar3{
+    	private int comment_id;
+    	private int movie1_id;
+    	private int user_id;
+    	private String usr_name;
+    	private String commennts;
+    	private double point;
+    	 kayitlar3(){
+    	    	
+    	    	
+    	    }
+    	    kayitlar3(int comment_id,int movie1_id,int user_id,String usr_name,String commennts,double point){
+    	    	this.comment_id=comment_id;
+    	    	this.movie1_id=movie1_id;
+    	    	this.user_id=user_id;
+    	    	this.usr_name=usr_name;
+    	    	this.commennts=commennts;
+    	    	this.point=point;
+    }
+			public int getComment_id() {
+				return comment_id;
+			}
+			public void setComment_id(int comment_id) {
+				this.comment_id = comment_id;
+			}
+			public int getMovie1_id() {
+				return movie1_id;
+			}
+			public void setMovie1_id(int movie1_id) {
+				this.movie1_id = movie1_id;
+			}
+			public int getUser_id() {
+				return user_id;
+			}
+			public void setUser_id(int user_id) {
+				this.user_id = user_id;
+			}
+			public String getUsr_name() {
+				return usr_name;
+			}
+			public void setUsr_name(String usr_name) {
+				this.usr_name = usr_name;
+			}
+			public String getCommennts() {
+				return commennts;
+			}
+			public void setCommennts(String commennts) {
+				this.commennts = commennts;
+			}
+			public double getPoint() {
+				return point;
+			}
+			public void setPoint(double point) {
+				this.point = point;
+			}
+    
+   
+    	
+    	
+    	
+    }
+    
+    
 
 }
